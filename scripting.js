721d7710edd598647996421eff0d3c5f6a124af3
chosunghoon <shcho@team-everywhere.com>
2024-11-05 18:23:19 +0900

    scripting 수정

--- scripting.js
+++ scripting.js
@@ -2,36 +2,38 @@ const extensions = 'https://developer.chrome.com/docs/extensions';
 const webstore = 'https://developer.chrome.com/docs/webstore';
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+  console.log('bugibugi');
+  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  // Next state will always be the opposite
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
-    if (nextState === 'OFF') {
-      // Remove the Bugi element
-      await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: () => {
-          window.bugi._destroy();
-          delete window.bugi;
-        },
-      });
-    } else {
-      chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        files: ['bugi.css'],
-      });
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['bugi.js'],
-      });
-    }
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
+  if (nextState === 'OFF') {
+    // Remove the Bugi element
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: () => {
+        window.bugi._destroy();
+        delete window.bugi;
+      },
+    });
+  } else {
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ['bugi.css'],
+    });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['bugi.js'],
     });
   }
+
+  // Set the action badge to the next state
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+  // if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+
+  // }
 });
