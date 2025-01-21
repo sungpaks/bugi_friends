16862b7e9f09b95db2b86e7402b366b469676b86
sungpaks <emforhs03150@gmail.com>
2025-01-21 21:45:30 +0900

    parseTrace.js 수정

--- parseTrace.js
+++ parseTrace.js
@@ -31,33 +31,9 @@ function analyzeTrace(data) {
 
   // 전체 실행 시간 계산
   const totalTime =
-    (events.reduce(
-      (max, event) => Math.max(max, event.ts + (event.dur || 0)),
-      0,
-    ) -
-      events.reduce((min, event) => Math.min(min, event.ts), Infinity)) /
-    1000; // ms
+    parseInt(data.metadata.modifications.initialBreadcrumb.window.range) / 1000; // ms
 
-  console.log(`Total Trace Time: ${(totalTime * 1000).toLocaleString()} ms`);
-
-  // 단위시간당 이벤트 수 계산
-  const layoutEventsPerSecond = layoutEvents.length / (totalTime / 1000);
-  const paintEventsPerSecond = paintEvents.length / (totalTime / 1000);
-  const layoutDurationPerSecond = layoutDuration / (totalTime / 1000);
-  const paintDurationPerSecond = paintDuration / (totalTime / 1000);
-
-  console.log(
-    `Layout Events per second: ${layoutEventsPerSecond.toFixed(5)} events/s`,
-  );
-  console.log(
-    `Paint Events per second: ${paintEventsPerSecond.toFixed(5)} events/s`,
-  );
-  console.log(
-    `Average Layout Time per second: ${layoutDurationPerSecond.toFixed(5)} ms/s`,
-  );
-  console.log(
-    `Average Paint Time per second: ${paintDurationPerSecond.toFixed(5)} ms/s`,
-  );
+  console.log(`Total Trace Time: ${totalTime.toLocaleString()} ms`);
 }
 
 // 분석 실행
